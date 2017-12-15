2b614900745b97f4bf90d25e85e93012115db822
Fady Naffa <fadynaffa3@gmail.com>
2017-12-15 15:48:46 +0200

    write specs for the draw functions

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -8,12 +8,10 @@ describe BitmapEditor do
     before(:each) { allow_any_instance_of(BitmapEditor).to receive(:validate_file!) }
 
     context 'create' do
-      it 'will create new image of 1 X 1 and set default value to O' do
+      it 'create a new M x N image with all pixels coloured white (O)' do
         allow(File).to receive(:open).and_return(['I 1 1', 'S'])
         expect { editor.run('somefile.txt') }.to output("O\n").to_stdout
-      end
 
-      it 'will create new image of 2 X 3 and set default value to 0' do
         allow(File).to receive(:open).and_return(['I 2 3', 'S'])
         expect { editor.run('somefile.txt') }.to output("O O\nO O\nO O\n").to_stdout
       end
@@ -30,9 +28,9 @@ describe BitmapEditor do
     end
 
     context 'change colour' do
-      it 'will create new image of 2 X 3 and set the value of 1,1 to B' do
-        allow(File).to receive(:open).and_return(['I 2 3', 'L 1 1 B' , 'S'])
-        expect { editor.run('somefile.txt') }.to output("B O\nO O\nO O\n").to_stdout
+      it 'colours the pixel (X,Y) with colour C' do
+        allow(File).to receive(:open).and_return(['I 2 3', 'L 1 1 C' , 'S'])
+        expect { editor.run('somefile.txt') }.to output("C O\nO O\nO O\n").to_stdout
       end
 
       it 'will return error when try to colour out of bound pixel' do
@@ -47,13 +45,67 @@ describe BitmapEditor do
     end
 
     context 'clear' do
-      it 'will reset image values to default' do
+      it 'clears the table, setting all pixels to white (O)' do
         allow(File).to receive(:open).and_return(['I 2 3', 'L 1 1 B' , 'C', 'S'])
         expect { editor.run('somefile.txt') }.to output("O O\nO O\nO O\n").to_stdout
       end
 
       it 'will return no image found when clear without image' do
         allow(File).to receive(:open).and_return(['C' , 'S'])
+        expect { editor.run('somefile.txt') }.to output("There is no image\n").to_stdout
+      end
+    end
+
+    context 'draw horizontal' do
+      it 'Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive)' do
+        allow(File).to receive(:open).and_return(['I 2 3', 'H 1 1 1 B' , 'S'])
+        # B O
+        # O O
+        # O O
+        expect { editor.run('somefile.txt') }.to output("B O\nO O\nO O\n").to_stdout
+
+        allow(File).to receive(:open).and_return(['I 5 4', 'H 2 3 3 B' , 'S'])
+        # O O O O O
+        # O O O O O
+        # O B B O O
+        # O O O O O
+        expect { editor.run('somefile.txt') }.to output("O O O O O\nO O O O O\nO B B O O\nO O O O O\n").to_stdout
+      end
+
+      it 'will return error when try to change an out of bound pixel' do
+        allow(File).to receive(:open).and_return(['I 2 3', 'H 5 5 5 B' , 'S'])
+        expect { editor.run('somefile.txt') }.to output("out of bound\n").to_stdout
+      end
+
+      it 'will return no image found when try to draw without image' do
+        allow(File).to receive(:open).and_return(['H 1 1 1 B' , 'S'])
+        expect { editor.run('somefile.txt') }.to output("There is no image\n").to_stdout
+      end
+    end
+
+    context 'draw vertical' do
+      it 'Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive)' do
+        allow(File).to receive(:open).and_return(['I 2 3', 'V 1 1 2 B' , 'S'])
+        # B O
+        # B O
+        # O O
+        expect { editor.run('somefile.txt') }.to output("B O\nB O\nO O\n").to_stdout
+
+        allow(File).to receive(:open).and_return(['I 5 4', 'V 2 3 3 B' , 'S'])
+        # O O O O O
+        # O O O O O
+        # O B O O O
+        # O O O O O
+        expect { editor.run('somefile.txt') }.to output("O O O O O\nO O O O O\nO B O O O\nO O O O O\n").to_stdout
+      end
+
+      it 'will return error when try to change an out of bound pixel' do
+        allow(File).to receive(:open).and_return(['I 2 3', 'V 5 5 5 B' , 'S'])
+        expect { editor.run('somefile.txt') }.to output("out of bound\n").to_stdout
+      end
+
+      it 'will return no image found when try to draw without image' do
+        allow(File).to receive(:open).and_return(['V 1 1 1 B' , 'S'])
         expect { editor.run('somefile.txt') }.to output("There is no image\n").to_stdout
       end
     end
